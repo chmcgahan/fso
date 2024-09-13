4d8781329725296812f9938ba52ab3c6eba64038
Charlie McGahan <chmcgahan@gmail.com>
2024-09-13 12:31:39 +0200

    using map or loop and combining variables into single object

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,6 +1,6 @@
-const Header = ({ course }) => {
+const Header = ({ name }) => {
   return (
-    <h1>{course}</h1>
+    <h1>{name}</h1>
   )
 }
 
@@ -11,7 +11,6 @@ const Part = ({ part }) => {
 const Content = ({ content }) => {
   
   const contentElements = [];
-
   for (let i = 0; i < content.length; i++){
     contentElements.push(
       <div key={i}>
@@ -25,7 +24,7 @@ const Content = ({ content }) => {
 
 const Total = ({ content }) => {
 
-  var total = 0;
+  let total = 0;
 
   for (let i = 0; i < content.length; i++){
     total = total + content[i].exercises;
@@ -37,19 +36,46 @@ const Total = ({ content }) => {
 
 }
 
-const App = () => {
-  const course = 'Half Stack application development'
+// const App = () => {
+//   const course = 'Half Stack application development'
   
-  const content_array = [
-    { name: "Fundamentals of React", exercises: 10 },
-    { name: "Using props to pass data", exercises: 7 },
-    { name: "State of a component", exercises: 14 }
-  ];
+//   const content_array = [
+//     { name: "Fundamentals of React", exercises: 10 },
+//     { name: "Using props to pass data", exercises: 7 },
+//     { name: "State of a component", exercises: 14 }
+//   ];
+//   return (
+//     <div>
+//       <Header course={course}/>
+//       <Content content={content_array}/>
+//       <Total content={content_array}/>
+//     </div>
+//   )
+// }
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  };
   return (
     <div>
-      <Header course={course}/>
-      <Content content={content_array}/>
-      <Total content={content_array}/>
+      <Header name={ course.name }/>
+      <Content content={ course.parts }/>
+      <Total content={ course["parts"] }/>
     </div>
   )
 }
